d2d33b9e44a25fb4c24100e6c3269ec5498a81b9
Oliver <oliver.ladores@wvsu.edu.ph>
2023-04-04 11:37:16 +0800

    Completed Exercise 4 Task 2.

--- E4/E4_2.cpp
+++ E4/E4_2.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
+#include <xstring>
 
 using namespace std;
 
 class DegreeAngle {
 private:
-  unsigned int angle;
+  int angle;
 
 public:
+  DegreeAngle() : angle(0){};
   DegreeAngle(unsigned int angle) {
     if (angle >= 360)
-      throw;
+      angle = angle % 360;
+    this->angle = angle;
+  }
+  friend auto operator+(DegreeAngle angle1, DegreeAngle angle2) -> const int {
+    return angle1.angle + angle2.angle;
+  }
+  friend auto operator-(DegreeAngle angle1, DegreeAngle angle2) -> const int {
+    return angle1.angle - angle2.angle;
   }
 };
+
+auto main() -> int {
+  auto angle1 = DegreeAngle(255);
+  auto angle2 = DegreeAngle(127);
+  cout << "Angle 1 + Angle 2: " << angle1 + angle2 << endl;
+  cout << "Angle 1 - Angle 2: " << angle1 - angle2 << endl;
+  return 0;
+}
